93804ccc6d003a04e8fd9cbbbc45a438c86aabf3
Mateusz Urban <mateusz.urban@ihs.com>
2014-08-17 18:32:16 +0200

    added more registry commands (before refactor)

--- models/cpu_commands/registry_operation_commands.rb
+++ models/cpu_commands/registry_operation_commands.rb
@@ -30,6 +30,18 @@ class CPU
 		self.PC += 2 unless registryValue == registryExpectedValue
 	end
 
+	#5xy0
+	#Skip next instruction if Vx = Vy.
+	def opcode_5XX0(opcode)
+		registryNumber1 = opcode[1]
+		registryNumber2 = opcode[2]
+		
+		registryValue1 = send("V#{registryNumber1}")
+		registryValue2 = send("V#{registryNumber2}")
+
+		self.PC += 2 if registryValue1 == registryValue2
+	end
+
 	#6xkk
 	#Set Vx = kk.
 	def opcode_6XXX(opcode)
@@ -37,6 +49,16 @@ class CPU
 		valueToSet = opcode[2..3].to_i(16)
 
 		send("V#{registryNumber}=", valueToSet)
+	end
+
+	#7xkk
+	#Set Vx = Vx + kk.
+	def opcode_7XXX(opcode)
+		registryNumber = opcode[1]
+		incrementBy = opcode[2..3].to_i(16)
+
+		registryValue = send("V#{registryNumber}")
+		send("V#{registryNumber}=", registryValue + incrementBy)
 	end
 
 	#8xy0
@@ -47,5 +69,88 @@ class CPU
 
 		registryToCopyFromValue = send("V#{registryToCopyFrom}")
 		send("V#{registryToSet}=", registryToCopyFromValue)
+	end
+
+	#8xy1
+	# Set Vx = Vx OR Vy.
+	def opcode_8XX1(opcode)
+		registerToSet = opcode[1]
+		registerToDoBitwiseOr = opcode[2]
+
+		registerToSetValue = send("V#{registerToSet}")
+		registerToDoBitwiseOrValue = send("V#{registerToDoBitwiseOr}")
+
+		valueToSet = registerToSetValue | registerToDoBitwiseOrValue
+		send("V#{registerToSet}=", valueToSet)
+	end
+
+	#8xy2
+	# Set Vx = Vx AND Vy.
+	def opcode_8XX2(opcode)
+		registerToSet = opcode[1]
+		registerToDoBitwiseAnd = opcode[2]
+
+		registerToSetValue = send("V#{registerToSet}")
+		registerToDoBitwiseAndValue = send("V#{registerToDoBitwiseAnd}")
+
+		valueToSet = registerToSetValue & registerToDoBitwiseAndValue
+		send("V#{registerToSet}=", valueToSet)
+	end
+
+	#8xy3
+	# Set Vx = Vx XOR Vy.
+	def opcode_8XX3(opcode)
+		registerToSet = opcode[1]
+		registerToDoBitwiseXor = opcode[2]
+
+		registerToSetValue = send("V#{registerToSet}")
+		registerToDoBitwiseXorValue = send("V#{registerToDoBitwiseXor}")
+
+		valueToSet = registerToSetValue ^ registerToDoBitwiseXorValue
+		send("V#{registerToSet}=", valueToSet)
+	end
+
+	#8xy4
+	# Set Vx = Vx + Vy.
+	#if result > 255 set VF to 1 (carry flag) otherwise set to 0
+	def opcode_8XX4(opcode)
+		registerToSet = opcode[1]
+		registerToAdd = opcode[2]
+
+		registerToSetValue = send("V#{registerToSet}")
+		registerToAddValue = send("V#{registerToAdd}")
+
+		result = registerToSetValue + registerToAddValue
+
+		if result > 255
+			self.VF = 1
+			result = result % 255
+		else
+			self.VF = 0
+		end	
+
+		send("V#{registerToSet}=", result)
+	end
+
+	#8xy5
+	# Set Vx = Vx - Vy.
+	# If Vx > Vy, then VF is set to 1, otherwise 0. Then Vy is subtracted from Vx, and the results stored in Vx.
+	def opcode_8XX5(opcode)
+		registerToSet = opcode[1]
+		registerToSubstract = opcode[2]
+
+		registerToSetValue = send("V#{registerToSet}")
+		registerToSubstractValue = send("V#{registerToSubstract}")
+
+		result = registerToSetValue + registerToAddValue
+
+		if result > 255
+			self.VF = 1
+			result = result % 255
+		else
+			self.VF = 0
+		end	
+
+		send("V#{registerToSet}=", result)
 	end
 end
--- specs/cpu_commands/registry_operation_commands_spec.rb
+++ specs/cpu_commands/registry_operation_commands_spec.rb
@@ -1,12 +1,18 @@
 require "./models/cpu"
 
+def memory_with_single_opcode(opcode)
+    memory = Memory.new
+
+    memory.load("200".hex, opcode[0..1].to_i(16))
+    memory.load("201".hex, opcode[2..3].to_i(16))
+
+    memory
+  end
+
 describe CPU, "registry operation commands" do
 
   it "can do jump instruction (1NNN)" do
-    memory = Memory.new
-
-    memory.load("200".hex, "12".to_i(16))
-    memory.load("201".hex, "30".to_i(16))
+    memory = memory_with_single_opcode("1230")
 
     cpu = CPU.new(memory)
     cpu.do_cycle
@@ -16,10 +22,7 @@ describe CPU, "registry operation commands" do
 
   #The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
   it "can do conditional jump instruction (3XKK) (condition true)" do
-    memory = Memory.new
-
-    memory.load("200".hex, "3B".to_i(16))
-    memory.load("201".hex, "89".to_i(16))
+    memory = memory_with_single_opcode("3B89")
 
     cpu = CPU.new(memory)
     cpu.VB = "89".to_i(16)
@@ -29,10 +32,7 @@ describe CPU, "registry operation commands" do
   end
 
   it "can do conditional jump instruction (3XKK) (condition false)" do
-    memory = Memory.new
-
-    memory.load("200".hex, "3B".to_i(16))
-    memory.load("201".hex, "89".to_i(16))
+    memory = memory_with_single_opcode("3B89")
 
     cpu = CPU.new(memory)
     cpu.VB = "15".to_i(16)
@@ -43,10 +43,7 @@ describe CPU, "registry operation commands" do
 
   #The interpreter compares register Vx to kk, and if they are NOT equal, increments the program counter by 2.
   it "can do conditional jump instruction (4XKK) (equal)" do
-    memory = Memory.new
-
-    memory.load("200".hex, "4C".to_i(16))
-    memory.load("201".hex, "89".to_i(16))
+    memory = memory_with_single_opcode("4C89")
 
     cpu = CPU.new(memory)
     cpu.VC = "89".to_i(16)
@@ -56,10 +53,7 @@ describe CPU, "registry operation commands" do
   end
 
   it "can do conditional jump instruction (4XKK) (not equal)" do
-    memory = Memory.new
-
-    memory.load("200".hex, "4C".to_i(16))
-    memory.load("201".hex, "89".to_i(16))
+    memory = memory_with_single_opcode("4C89")
 
     cpu = CPU.new(memory)
     cpu.VC = "15".to_i(16)
@@ -68,26 +62,52 @@ describe CPU, "registry operation commands" do
     expect(cpu.PC).to eq("204".to_i(16))
   end
 
+  it "can do conditional jump instruction (5XY0) (equal)" do
+    memory = memory_with_single_opcode("5EF0")
+
+    cpu = CPU.new(memory)
+    cpu.VE = 1
+    cpu.VF = 1
+    cpu.do_cycle
+
+    expect(cpu.PC).to eq("204".to_i(16))
+  end
+
+  it "can do conditional jump instruction (5XY0) (not equal)" do
+    memory = memory_with_single_opcode("5280")
+
+    cpu = CPU.new(memory)
+    cpu.V2 = 1
+    cpu.V8 = 2
+    cpu.do_cycle
+
+    expect(cpu.PC).to eq("202".to_i(16))
+  end
 
   it "can set VX registers (6XKK)" do
-  	memory = Memory.new
-
-  	value_to_set = "58".to_i(16)
-  	memory.load("200".hex, "65".to_i(16))
-  	memory.load("201".hex, value_to_set)
+  	memory = memory_with_single_opcode("6558")
 
     cpu = CPU.new(memory)
     cpu.do_cycle
 
-    expect(cpu.V5).to eq(value_to_set)
+    expect(cpu.V5).to eq("58".to_i(16))
+  end
+
+  it "can increment registry by specified value (7XKK)" do
+    memory = memory_with_single_opcode("7B32")
+
+    cpu = CPU.new(memory)
+    cpu.VB = "20".to_i(16)
+    expected = "20".to_i(16) + "32".to_i(16)
+
+    cpu.do_cycle
+
+    expect(cpu.VB).to eq(expected)
   end
 
   it "can copy between registers (8XY0)" do
-  	memory = Memory.new
-
   	#V2 = VA
-  	memory.load("200".hex, "82".to_i(16))
-  	memory.load("201".hex, "A0".to_i(16))
+    memory = memory_with_single_opcode("82A0")
 
     cpu = CPU.new(memory)
     cpu.VA = 56
@@ -96,4 +116,100 @@ describe CPU, "registry operation commands" do
 
     expect(cpu.V2).to eq(cpu.VA)
   end
+
+  it "can do bitwise OR on registers (8XY1)" do
+    #V3 = V3 OR V4
+    memory = memory_with_single_opcode("8341")
+
+    cpu = CPU.new(memory)
+    cpu.V3 = 56
+    cpu.V4 = 88
+
+    cpu.do_cycle
+
+    expect(cpu.V3).to eq(120)
+  end
+
+  it "can do bitwise AND on registers (8XY2)" do
+    #V7 = V7 AND V1
+    memory = memory_with_single_opcode("8712")
+
+    cpu = CPU.new(memory)
+    cpu.V7 = 20
+    cpu.V1 = 30
+
+    cpu.do_cycle
+
+    expect(cpu.V7).to eq(20)
+  end
+
+  it "can do bitwise XOR on registers (8XY3)" do
+    #V4 = V4 XOR VA
+    memory = memory_with_single_opcode("84A3")
+
+    cpu = CPU.new(memory)
+    cpu.V4 = 27
+    cpu.VA = 28
+
+    cpu.do_cycle
+
+    expect(cpu.V4).to eq(7)
+  end
+
+  it "can add registers each other (8XY4) (without overflowing 8-bits)" do
+    #V1 = V1 + V2
+    memory = memory_with_single_opcode("8124")
+
+    cpu = CPU.new(memory)
+    cpu.V1 = 20
+    cpu.V2 = 30
+
+    cpu.do_cycle
+
+    expect(cpu.V1).to eq(50)
+    expect(cpu.VF).to eq(0)
+  end
+
+  it "can add registers each other (8XY4) (with overflowing 8-bits)" do
+    #V1 = V1 + V2
+    memory = memory_with_single_opcode("8124")
+
+    cpu = CPU.new(memory)
+    cpu.V1 = 200
+    cpu.V2 = 56
+
+    cpu.do_cycle
+
+    expect(cpu.V1).to eq(1)
+    expect(cpu.VF).to eq(1)
+  end
+
+  it "can substract registers each other (8XY5) (Vx > Vy)" do
+    #V1 = V1 - V2
+    memory = memory_with_single_opcode("8125")
+
+    cpu = CPU.new(memory)
+    cpu.V1 = 200
+    cpu.V2 = 70
+
+    cpu.do_cycle
+
+    expect(cpu.V1).to eq(130)
+    expect(cpu.VF).to eq(1)
+  end
+
+  it "can substract registers each other (8XY5) (Vx < Vy)" do
+    #V1 = V1 - V2
+    memory = memory_with_single_opcode("8125")
+
+    cpu = CPU.new(memory)
+    cpu.V1 = 70
+    cpu.V2 = 200
+
+    cpu.do_cycle
+
+    expect(cpu.V1).to eq(130)
+    expect(cpu.VF).to eq(0)
+  end
+
 end
